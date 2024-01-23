546816a7739fe808b4798d83001588342ee5f1fa
Nicolas Hak <nicolas.l.hak@gmail.com>
2024-01-23 12:02:44 -0800

    Initial commit

---
+++ my-react-app/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+
+const app = express();
+
+// Parse incoming JSON data
+app.use(bodyParser.json());
+
+// Serve static assets from the React app's build directory
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Serve the React app's index.html at the root path
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// New route for handling GET requests to the root path
+app.get('/', (req, res) => {
+    res.send('Hello from the server!'); // Or any response you want
+});
+
+app.get("/foo", (req, res) => {
+    res.send("You've requested the foo page.");
+});
+
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
