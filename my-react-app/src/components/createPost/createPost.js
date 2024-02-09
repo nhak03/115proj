6acd6faa1da162528adb94c69f213e65511b45e2
Bora Dursun <borakdursun@gmail.com>
2024-02-09 11:45:21 -0800

    added the create post button

---
+++ my-react-app/src/components/createPost/createPost.js
@@ -0,0 +1,127 @@
+//
+// Creating Posts
+//
+
+import React, { useState } from 'react';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+
+const CreatePost = () =>
+{
+    // Fields for post contents
+    const [ organizationName, setOrganizationName ] = useState('');
+    const [    postTitle    ,    setPostTitle     ] = useState('');
+    const [ postDescription , setPostDescription  ] = useState('');
+    
+    // TODO: Optional other fields
+    // const[ ... ]
+
+
+    const [  errorMessage  ,   setErrorMessage  ] = useState('');
+    const [   submitted    ,    setSubmitted    ] = useState(false);
+
+
+    const handleSubmit = async (e) =>
+    {
+        e.preventDefault();
+    
+        // Validation: Ensure both fields are filled out
+        if (!organizationName || !postTitle || !postDescription)
+        {
+            setErrorMessage('Please fill out all fields.');
+            return;
+        };
+
+        //
+        // TODO: Send stuff to backend
+        //
+    
+        // Clear the form
+        setOrganizationName('');
+        setPostTitle('');
+        setPostDescription('');
+        setErrorMessage('');
+        setSubmitted(true);
+    };
+
+    return (
+        <div>
+          <Popup
+            trigger = {<button> Create a post </button>}
+            modal
+            nested
+          >
+            {close => (
+              <div className = 'modal'>
+                <button className = "close" onClick = {close}>
+                  &times;
+                </button>
+                <div>
+                {/* Check if post has been submitted */}
+                  {submitted ? (
+                    <div>
+                      <h2>Success!</h2>
+                      <p>Post created</p>
+                    </div>
+                  ) : (
+                    <div>
+                        {/* Prompt user with the post creation form */}
+                        <h2>Fill out the form to create a post</h2>
+                        <form onSubmit={handleSubmit}>
+                        
+
+                        {/* Fill in the necessary fields */}
+
+                        {/* Organization name field */}
+                        <div>
+                            <label htmlFor = "organizationName">Organization name:</label>
+                            <input 
+                            type     = "text"
+                            id       = "organizationName" 
+                            value    = {organizationName} 
+                            onChange = {(e) => setOrganizationName(e.target.value)} 
+                            />
+                        </div>
+
+                        {/* Post title field */}
+                        <div>
+                            <label htmlFor = "postTitle">Post title:</label>
+                            <input 
+                            type     = "text" 
+                            id       = "postTitle" 
+                            value    = {postTitle} 
+                            onChange = {(e) => setPostTitle(e.target.value)} 
+                            />
+                        </div>
+
+                        {/* Post description field */}
+                        <div>
+                            <label htmlFor = "postDescription">Post description:</label>
+                            <input 
+                            type     = "text" 
+                            id       = "postDescription" 
+                            value    = {postDescription} 
+                            onChange = {(e) => setPostDescription(e.target.value)} 
+                            />
+                        </div>
+                        {/* End of fields */}
+
+                        {/* Handle post submission */}
+                        <button type = "submit">Create post</button>
+                        </form>
+                    </div>
+                  )}
+                  <div/>
+                  {errorMessage && <p>{errorMessage}</p>}         
+                </div>
+                <div>
+                  <button onClick={() => close()}>Cancel</button>
+                </div>
+              </div>
+            )}
+          </Popup>
+        </div>
+      );      
+};
+
+export default CreatePost
