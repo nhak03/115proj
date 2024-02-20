bed11a2219daff87a2231a48db7bc0eab17e5f4d
Ronith <ronith2k@gmail.com>
2024-02-19 17:33:30 -0800

    refactored useAuthState code

---
+++ my-react-app/src/components/auth/useAuthState.js
@@ -0,0 +1,21 @@
+// useAuthState.js
+import { useEffect, useState } from 'react';
+import { auth } from '../../firebase.js';
+import { onAuthStateChanged } from 'firebase/auth';
+
+const useAuthState = () => {
+    const [authUser, setAuthUser] = useState(null);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, user => {
+            setAuthUser(user);
+        });
+
+        // Clean up the subscription on unmount
+        return unsubscribe;
+    }, []);
+
+    return authUser;
+};
+
+export default useAuthState;
