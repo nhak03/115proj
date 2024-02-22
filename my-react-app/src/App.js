452c74fde8f7e56773aca9095c529fa18317a5ed
Jack Kelly <jawkelly@ucsc.edu>
2024-02-21 17:55:09 -0800

    Post css and club_page routing

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,39 +10,24 @@ import SignIn from './components/auth/SignIn.jsx';
 import SignUp from './components/auth/SignUp.jsx';
 import AuthDetails from './components/auth/AuthDetails.jsx';
 import Discover from './pages/Discover.js';
-
+import Profile from './pages/Profile.js'
+import ClubPage from './pages/ClubPage.js'
 import WhatsNew from './pages/WhatsNew.js';
 
 
 function App() {
-  // const [showHello, setShowHello] = useState(false);
   
 
   return (
-
-    // SHOULD BE UNCOMMENTED AFTER LOGIN STUFF COMPLETE
-
       <div className="App">
-        
-        {/* <div>
-          <Header />
-          <div>
-            {posts.map(post => (
-              <Post
-                key={post.id}
-                title={post.title}
-                timestamp={new Date(post.timestamp).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric' })}
-                content={post.content}
-              />
-            ))}
-          </div>
-        </div> */}
         <BrowserRouter>
         <div>
           <Routes>
             <Route path="/" element={<WhatsNew/>} />
             <Route path="/following" element={<Following/>} />
             <Route path='/discover' element={<Discover/>}/>
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/club_page/:clubName" element={<ClubPage />} />
           </Routes>
         </div>
         </BrowserRouter>
