35f2d31165d267a9376f243406804bdc8829fbb2
Mohan Duvvuri <mohanduvvuri@Mohans-MacBook-Pro.local>
2024-03-05 12:20:29 -0800

    did some work on ui for login/signup button

--- my-react-app/src/components/Header/Header.js
+++ my-react-app/src/components/Header/Header.js
@@ -1,40 +1,57 @@
 // Header.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import logo from './SL_logo.png'; // Update the path
-import CreatePost from '../createPost/createPost.js';
-// import '../createPost/createPost.css';
+import logo from './SL_logo.png';
+import Modal from '../Modal.js';
 import useAuthState from '../auth/useAuthState.js';
+import SignIn from '../auth/SignIn.jsx';
+import SignUp from '../auth/SignUp.jsx';
+import AuthDetails from '../auth/AuthDetails.jsx';
+import CreatePost from '../createPost/createPost.js';
 
 const Header = () => {
+  const [showModal, setShowModal] = useState(false);
   const authUser = useAuthState();
-  
+
+  const handleOpenModal = () => setShowModal(true);
+  const handleCloseModal = () => setShowModal(false);
+
   return (
-    <header>
-      <img src={logo} alt="SlimeLine Logo" className="logo" />
-      <h1>SlimeLine</h1>
-      <nav>
+    <header className="header">
+      <div className="top-bar">
+        <div className="logo-container">
+          <img src={logo} alt="SlimeLine Logo" className="logo" />
+        </div>
+        <h1>SlimeLine</h1>
+        <div className="auth-controls">
+          {authUser ? (
+            <>
+              <AuthDetails />
+              {/* Implement the signOut function and pass it to AuthDetails if needed */}
+            </>
+          ) : (
+            <button onClick={handleOpenModal}>Log In / Sign Up</button>
+          )}
+        </div>
+      </div>
+      <nav className="navigation">
         <ul>
           <li><a href="/">What's New</a></li>
           <li><a href="/following">Following</a></li>
           <li><a href="/discover">Discover</a></li>
+          {authUser && authUser.clubStatus && <CreatePost />}
         </ul>
       </nav>
 
-      {
-        /*
-        Conditionally render the CreatePost button if and only if a user is authenticated
-        And they are a club account as well.
-        Extra error handling contained within CreatePost object
-        */
-      }
-      <div>
-        {authUser && authUser.clubStatus && <CreatePost/>}
-      </div>
-      
+      {showModal && (
+        <Modal onClose={handleCloseModal}>
+          <SignIn />
+          <SignUp />
+        </Modal>
+      )}
     </header>
   );
-}
+};
 
 export default Header;
